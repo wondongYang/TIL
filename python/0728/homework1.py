458fecf07e8a4079987812e17ea9282c62e1f305
wondong Yang <ehd8331@gmail.com>
2021-07-28 22:11:05 +0900

    python module

---
+++ python/0728/homework1.py
@@ -0,0 +1,8 @@
+# Type Class
+
+print(type(list))
+print(type(dict))
+print(type(set))
+print(type(tuple))
+print(type(str))
+print(type(int))
---
+++ python/0728/homework3.py
@@ -0,0 +1,16 @@
+from abc import abstractmethod
+
+
+# Instance Method
+
+# print(dir(str))
+str_a = 'abcde'
+print(str_a.upper())
+
+# print(dir(list))
+list_a = [1, 2, 3, 4, 5, 2, 2, 2]
+print(list_a.count(2))
+
+# print(dir(dict))
+dict_a = {'1':2, '2':3, '3':4}
+print(dict_a.get('1'))
