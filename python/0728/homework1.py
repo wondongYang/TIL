b440ec10b04b12c7d4970d06c00502e3f2f90784
wondong Yang <ehd8331@gmail.com>
2021-08-02 21:25:28 +0900

    web 맛보기2

--- python/0728/homework1.py
+++
@@ -1,8 +0,0 @@
-# Type Class
-
-print(type(list)) #=> <class 'type'>
-print(type(dict)) #=> <class 'type'>
-print(type(set)) #=> <class 'type'>
-print(type(tuple)) #=> <class 'type'>
-print(type(str)) #=> <class 'type'>
-print(type(int)) #=> <class 'type'>
