2eb4a93c1d5317a242cbe8647faef2917f11b76e
wondong Yang <ehd8331@gmail.com>
2021-08-31 21:09:32 +0900

    0831 django

---
+++ 3. django/0831/django project/articles/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('index/', views.index, name='index'),
+    path('introduce/', views.introduce, name='introduce'),
+    path('greeting/', views.greeting, name='greeting'),
+    path('dinner/', views.dinner, name='dinner'),
+    path('image/', views.image),
+    path('template_language/', views.template_language),
+    path('throw/', views.throw),
+    path('catch/', views.catch),
+    path('hello/<str:name>/', views.hello),
+    # path('hello/<int:number>/', views.hello),
+]
