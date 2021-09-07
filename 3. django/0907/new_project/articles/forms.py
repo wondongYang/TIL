c9783e47663ab25315f24820cfbc3ac58aec0bbb
wondong Yang <ehd8331@gmail.com>
2021-09-07 17:56:05 +0900

    django crud

---
+++ 3. django/0907/new_project/articles/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Article
+
+class ArticleForm(forms.ModelForm):
+
+    class Meta: # form에 대한 정보
+        model = Article
+        fields = '__all__'
+        # fields = ['title', 'content'] # 전부 다 적어주는 걸 권장함
+        # fields = ('title', 'content')
+        # exclude = ('title', ) 
+        # exclude -> title 빼고 -> 필드가 100개 있다면 title 빼고, 99개가 나옴
+        # 참고로 ('title') -> 문자열이다. ('title') == 'title' 튜플로 쓰려면 ,를 찍어줘야함!
---
+++ 3. django/0907/new_project/articles/views.py
@@ -0,0 +1,75 @@
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_http_methods, require_POST, require_safe
+
+from .models import Article
+from .forms import ArticleForm
+
+# Create your views here.
+@require_safe
+def index(request):
+    articles = Article.objects.all()
+    context = {
+        'articles': articles
+    }
+    return render(request, 'articles/index.html', context)
+
+@require_http_methods(['GET', 'POST'])
+def create(request):
+    # 5. create 경로로 요청이 들어옴(POST) [case1. 잘못된 입력] => 사용자가 데이터 입력
+    # 10. create 경로로 요청이 들어옴(POST) [case2. 올바른 입력] => 사용자가 데이터 입력
+    if request.method == 'POST':
+        # 6. 데이터가 입력된 종이를 가져옴 => ArticleForm으로 인스턴스화(빈 종이 + 사용자 데이터) 
+        # 11. 데이터가 입력된 종이를 가져옴 => ArticleForm으로 인스턴스화(빈 종이 + 사용자 데이터) 
+        form = ArticleForm(data=request.POST) # data= 생략 가능
+        # 7. 사용자가 입력한 데이터가 유효한지(올바른지) 확인
+        # 12. 사용자가 입력한 데이터가 유효한지(올바른지) 확인
+        if form.is_valid():
+            # 13. 데이털르 db에 저장한다.
+            form.save()
+            # 14. index로 리다이렉트 시켜준다.
+            return redirect('articles:index')
+
+    else: # 1. create 경로로 요청이 들어옴(GET-DB에 영향 X) -> 빈 종이(Form) 응답
+        form = ArticleForm # 2. ArticleForm으로 빈 종이 생성(인스턴스 생성)
+
+    # 3. 사용자에게 빈종이를 주기 위해서 context에 form을 담는다.
+    # 8. 잘못된 데이터를 다시 입력 받기 위해서 context에 form을 담는다.
+    context = { 
+        'form': form,
+    }
+
+    # 4. 사용자에게 데이터를 받기 위해 빈 종이를 넘겨준다. -> 데이터를 입력한다.
+    # 9. 사용자에게 올바른 데이터를 받기 위해 form을 넘겨준다.
+    return render(request, 'articles/create.html', context)
+
+@require_safe
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article': article,
+    }
+    return render(request, 'articles/detail.html', context)
+
+@require_POST # a태그를 form으로 변경 필요.
+def delete(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.delete()
+    return redirect('articles:index')
+
+@require_http_methods(['GET', 'POST'])
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            article = form.save() # article에 수정 완료된 데이터 1줄이 들어감
+            return redirect('articles:detail', article.pk)
+
+    else: # GET 요청
+        form = ArticleForm(instance=article)
+    
+    context = {
+        'article': article, # article.pk를 a태그에 사용하기 위해
+        'form': form,
+    }
+    return render(request, 'articles/update.html', context)
