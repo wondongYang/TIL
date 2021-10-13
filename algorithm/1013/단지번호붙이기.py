801454bdff1cd7691c4e42811303d1281abc53d2
wondong Yang <ehd8331@gmail.com>
2021-10-13 13:22:18 +0900

    algorithm 단지번호붙이기bfs

--- algorithm/1013/단지번호붙이기.py
+++ algorithm/1013/단지번호붙이기.py
@@ -7,6 +7,21 @@ def dfs1(i, j, N):
         if 0 <= ni < N and 0 <= nj < N and arr[ni][nj] == 1 and visited[ni][nj] == 0:
             dfs1(ni, nj, N)
 
+def bfs(i, j, N):
+    cnt = 0
+    q = [(i, j)]        # 큐생성, 시작점 인큐
+    visited[i][j] = 1   # 시작점 방문표시
+    while q:
+        i, j = q.pop(0)
+        # visit(i, j)
+        cnt += 1
+        for di, dj in [[0, 1], [1, 0], [0, -1], [-1, 0]]:
+            ni, nj = i + di, j + dj
+            if 0 <= ni < N and 0 <= nj < N and arr[ni][nj] == 1 and visited[ni][nj] == 0:
+                q.append((ni, nj))
+                visited[ni][nj] = 1
+    return cnt
+
 
 N = int(input())
 arr = [list(map(int, input())) for _ in range(N)]
@@ -17,9 +32,10 @@ block = []
 for i in range(N):
     for j in range(N):
         if not visited[i][j] and arr[i][j] == 1:
-            cnt = 0
             ans += 1
-            dfs1(i, j, N)
+            # cnt = 0
+            # dfs1(i, j, N)
+            cnt = bfs(i, j, N)
             block.append(cnt)
 
 print(ans)
