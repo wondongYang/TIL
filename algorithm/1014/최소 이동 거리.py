ed6abfd68f8793f5a1d65c2556b073cf70197e2b
wondong Yang <ehd8331@gmail.com>
2021-10-14 17:49:19 +0900

    algorithm 최소 이동 거리

---
+++ algorithm/1014/최소 이동 거리.py
@@ -0,0 +1,34 @@
+def dijkstra(s, V):
+    U = [0]*(V+1)       # 비용이 결정된 정점을 표시
+    U[s] = 1            # 출발점 비용 결정
+    for i in range(V+1):
+        D[i] = adjM[s][i]
+        
+    # 남은 정점의 비용 결정
+    for _ in range(V):      # 남은 정점 개수만큼 반복
+        # D[w]가 최소인 w 결정, 비용이 결정되지 않는 정점w 중에서 
+        minV = INF
+        w = 0
+        for i in range(V+1):
+            if U[i] == 0 and minV > D[i]:
+                minV = D[i]
+                w = i
+        U[w] = 1                # 비용 결정
+        for v in range(V+1):
+            if 0 < adjM[w][v] < INF:
+                D[v] = min(D[v], D[w] + adjM[w][v])
+
+T = int(input())
+for tc in range(T):
+    INF = 10000
+    V, E = map(int, input().split())
+    adjM = [[INF]*(V+1) for _ in range(V+1)]
+    for i in range(V+1):
+        adjM[i][i] = 0
+    for _ in range(E):
+        u, v, w = map(int, input().split())
+        adjM[u][v] = w
+
+    D = [0]*(V+1)
+    dijkstra(0, V)
+    print(f'{tc+1} {D}')
