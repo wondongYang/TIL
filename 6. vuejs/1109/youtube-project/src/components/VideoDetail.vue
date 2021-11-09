861dce8331ae9ce6ef1967c4bda6644a5597e31f
wondong Yang <ehd8331@gmail.com>
2021-11-09 17:02:36 +0900

    vue Youtube_project

---
+++ 6. vuejs/1109/youtube-project/src/components/VideoDetail.vue
@@ -0,0 +1,35 @@
+<template>
+  <div v-if="video">
+    <iframe :src="videoURL" frameborder="0"></iframe>
+    <h2>title: {{ video.snippet.title | stringUnescape }}</h2>
+    <p>description: {{ video.snippet.description | stringUnescape }}</p>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+
+export default {
+  name: 'VideoDetail',
+  props: {
+    video: {
+      type: [String, Object], //처음에 아무것도 안고른 경우, 골랐을 경우
+    }
+  },
+  computed: {
+    videoURL: function () {
+      const videoId = this.video.id.videoId
+      return `https://www.youtube.com/embed/${videoId}`
+    }
+  },
+  filters: {
+    stringUnescape: function (rawText) {
+      return _.unescape(rawText)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
