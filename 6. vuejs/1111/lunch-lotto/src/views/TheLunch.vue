dc71d99cb2659f88a6a79bd82af8bdc07c9b3bdc
wondong Yang <ehd8331@gmail.com>
2021-11-11 10:40:01 +0900

    vue router lunch_lotto

---
+++ 6. vuejs/1111/lunch-lotto/src/views/TheLunch.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h2>점심메뉴 추천</h2>  
+    <button @click="pickOneInLunchMenu">점심 뭐먹지...?</button>
+    <h2>오늘의 점심 메뉴는: {{ selectedLunchMenu }}</h2>
+    <hr>
+    <h3>로또 숫자 갯수 입력</h3>
+    <input v-model="lottoNum" placeholder="추천 받고 싶은 로또 숫자 갯수 입력" type="text">
+    <br><br>
+    <button @click="gotoLotto">로또 하러 가즈아~</button>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+
+export default {
+  name: 'TheLunch',
+  data: function () {
+    return {
+      lunch: ['국밥', '짜장면', '햄버거'],
+      selectedLunchMenu: '',
+      lottoNum: 0,
+    }  
+  },
+  methods: {
+    pickOneInLunchMenu: function () {
+      const randomIndex = _.random(this.lunch.length - 1)
+      this.selectedLunchMenu = this.lunch[randomIndex]
+    },
+    gotoLotto: function () {
+      // this.$router.push('/lotto')
+      this.$router.push({ name: 'TheLotto', params: { lottoNum: this.lottoNum }})
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
